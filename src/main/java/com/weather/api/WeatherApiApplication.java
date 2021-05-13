d60af6e21cfc1abb3c14900e3c1f80a6a1f13734
ryzenboi98 <joaopro23@gmail.com>
2021-05-13 11:28:14 +0100

    Change

--- src/main/java/com/weather/api/WeatherApiApplication.java
+++ src/main/java/com/weather/api/WeatherApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WeatherApiApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
 	}
